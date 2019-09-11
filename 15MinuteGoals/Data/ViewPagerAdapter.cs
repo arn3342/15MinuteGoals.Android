9f1672f8de71627f085272a0f81c640f5f417a91
Aousaf Rashisd <nabilsakibiram@gmail.com>
2019-09-11 13:40:21 +0600

    Added viewPagerAdapter

---
+++ 15MinuteGoals/Data/ViewPagerAdapter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Android.Support.V4.App;
+
+namespace _15MinuteGoals.Data
+{
+    public class ViewPagerAdapter : FragmentStatePagerAdapter
+    {
+        public List<Fragment> FragmentList = new List<Fragment>();
+
+        public ViewPagerAdapter(FragmentManager fm) : base(fm)
+        {
+
+        }
+        public override int Count
+        {
+            get { return FragmentList.Count; }
+        }
+
+        public override Fragment GetItem(int position)
+        {
+            return FragmentList[position];
+        }
+        public ViewPagerAdapter(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public void AddFragment(Fragment fragment)
+        {
+            FragmentList.Add(fragment);
+        }
+    }
+}
