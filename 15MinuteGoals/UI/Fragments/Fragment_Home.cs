436d14941fc5ae3cbcc6a84d8c65c116b18f5176
Aousaf Rashisd <nabilrashid44@gmail.com>
2019-09-26 19:23:53 +0600

    New design implemented

--- 15MinuteGoals/UI/Fragments/Fragment_Home.cs
+++ 15MinuteGoals/UI/Fragments/Fragment_Home.cs
@@ -1,10 +1,12 @@
 ﻿using _15MinuteGoals.Activities;
+using _15MinuteGoals.UI.AnimationClasses;
 using _15MinuteGoals.UI.CustomViews;
 using Android.Content;
 using Android.Graphics.Drawables;
 using Android.OS;
 using Android.Support.V4.App;
 using Android.Views;
+using Android.Views.Animations;
 using Android.Widget;
 using FFImageLoading;
 using System;
@@ -19,6 +21,7 @@ namespace _15MinuteGoals.UI.Fragments
         public static int mProfileBoxHeight { get; internal set; }
         public static TopBar topbar { get; private set; }
         public Button pursueButton { get; set; }
+        private View AcademicBtn, EntrepreneurBtn;
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
@@ -26,10 +29,32 @@ namespace _15MinuteGoals.UI.Fragments
             //topbar = myview.FindViewById<TopBar>(Resource.Id.home_topbar);
             //pursueButton = myview.FindViewById<Button>(Resource.Id.pursuebutton);
             //pursueButton.Click += PursueButton_Click;
-
+            AcademicBtn = myview.FindViewById<View>(Resource.Id.academicBtn);
+            EntrepreneurBtn = myview.FindViewById<View>(Resource.Id.entrepreneurBtn);
+            AcademicBtn.Click += AcademicBtn_Click;
+            EntrepreneurBtn.Click += EntrepreneurBtn_Click;
             //SetTopBar();
             //Fix scaling
             return myview;
+        }
+
+        private void EntrepreneurBtn_Click(object sender, EventArgs e)
+        {
+            AnimateButtonClick(EntrepreneurBtn);
+        }
+
+        private void AcademicBtn_Click(object sender, EventArgs e)
+        {
+            AnimateButtonClick(AcademicBtn);
+        }
+
+        void AnimateButtonClick(View view)
+        {
+            Context context = view.Context;
+            Animation anim = AnimationUtils.LoadAnimation(context, Resource.Animation.bounceScaleAnimation);
+            ScaleBounceInterpolator scaleBounceInterpolator = new ScaleBounceInterpolator(0.2, 5);
+            anim.Interpolator = scaleBounceInterpolator;
+            view.StartAnimation(anim);
         }
 
         private void PursueButton_Click(object sender, EventArgs e)
